e292e5f79174f9b1a71dc21ea9d584881bb52794
timothyAltonji <77422130+timothyAltonji@users.noreply.github.com>
2022-01-25 10:42:56 -0500

    AStar changes

--- AStar.java
+++ AStar.java
@@ -1,4 +1,23 @@
+/*
+*       start        x       target
+*         |----------|---------|
+*              g(x)      h(x)
+*         |--------------------|
+*                   f(x) 
+*/ 
+
+
 public class AStar 
 {
-    
+    private  Point start;
+    private  Point target;
+    private  int NodeCount;
+    private  Point[] graph; 
+
+    AStar()
+    {
+
+    }
 }
+
+
--- ThetaStar.java
+++ ThetaStar.java
@@ -1,3 +1,4 @@
-public class ThetaStar {
-    
+public class ThetaStar 
+{
+        
 }
