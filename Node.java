0a3d2c75c83e86b6d519da8d708f67f4f4b3e8e9
timothyAltonji <77422130+timothyAltonji@users.noreply.github.com>
2022-01-25 11:05:48 -0500

    Node

--- Node.java
+++ Node.java
@@ -1,17 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.awt.Point;
+import java.util.PriorityQueue;
 
 public class Node implements Comparable<Node> 
 {
     public Node parent = null;
-
     public List<Edge> neighbors;
-
+    public Point h; 
+    
     public Point f;
     public Point g;
-    
-    public Point h; 
 
     Node(Point h)
     {
@@ -19,6 +18,7 @@ public class Node implements Comparable<Node>
         this.neighbors = new ArrayList<>();
     }
 
+    //allow overide to see target and start points
     @Override
     public int compareTo(Node n)
     {
@@ -26,10 +26,9 @@ public class Node implements Comparable<Node>
         int y = Double.compare(this.f.getY(), n.f.getY());
         
         return (x,y);
-         
-         
     }
 
+    //shortest grid path
     public static class Edge 
     {
         Edge(int weight, Node node)
@@ -51,6 +50,7 @@ public class Node implements Comparable<Node>
     //h()
     public double calculateHeuristic(Node target) 
     {
+        //sequential measuring of distance between points to target.
         return this.h;
     }
 }
