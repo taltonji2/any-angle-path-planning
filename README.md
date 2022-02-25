acf17619b9e26f51da08b958e0ab30fa6688312d
timothyAltonji <77422130+timothyAltonji@users.noreply.github.com>
2022-02-24 22:37:52 -0500

    final submission

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 Hello
 ----------------------------------------------------------------------------------------
 TO LOAD GRID:
-To load a new text file, for testing it must be located in resources and named grid0.txt
+To load a new text file, for testing it must be located outside the src folder and named grid0.txt.
 To generate a grid specified to 100 x 50 cells with random blocked cells of 10% the total cell count, run CreateInput.java
 
 TO RUN APPLICATION:
-Compile ArtificalIntel and run App.java
+Compile src folder and run App.java
+
+A prompt will appear in terminal asking to select an algorithm to run.
+1 for A* 2 for Theta*.
--- src/App.java
+++ src/App.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // System.out.println("\n");
-        // System.out.println("Choose an option: ");
-        // System.out.println("1: A*");
-        // System.out.println("2: Theta*");
-        // System.out.println("\n");
+         System.out.println("\n");
+        System.out.println("Choose an option: ");
+         System.out.println("1: A*");
+        System.out.println("2: Theta*");
+        System.out.println("\n");
 
-        // BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        // String response = reader.readLine();
-        // if(response.equals("1"))
-        // {
-        //     reader.close();
+       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String response = reader.readLine();
+        if(response.equals("1"))
+        {
+         reader.close();
             String currentDirectory = System.getProperty("user.dir");
             String fileName = currentDirectory + "/grid0.txt";
             Grid grid = GridStorage.restoreGrid(fileName);
@@ -22,36 +22,35 @@ public class App {
             
             BFS bfs = new BFS();
             AStar a = new AStar();
-            ThetaStar t = new ThetaStar();
+            
             if(bfs.doBFS(grid))
             {
-                ArrayList<Cell> closedList = t.doThetaStar(grid);
+                ArrayList<Cell> closedList = a.doAStar(grid);
                 DisplayGrid displayGrid = new DisplayGrid(grid, "1", closedList);
                 displayGrid.paintPath(closedList);
             } else {
                 System.out.println("No Such Path");
             }
-        // }
-        // if(response.equals("2"))
-        // {
-        //     reader.close();
-    //         String currentDirectory = System.getProperty("user.dir");
-    //         String fileName = currentDirectory + "/grid0.txt";
-    //         Grid grid = GridStorage.restoreGrid(fileName);
-    //         grid.createEdgeCells();
-    //         grid.LoadAdj();
+        }
+         else if(response.equals("2"))
+        {
+         reader.close();
+          String currentDirectory = System.getProperty("user.dir");
+          String fileName = currentDirectory + "/grid0.txt";
+           Grid grid = GridStorage.restoreGrid(fileName);
+         grid.LoadAdj();
             
             
-    //         BFS bfs = new BFS();
-    //         ThetaStar t = new ThetaStar();
-    //         if(bfs.doBFS(grid))
-    //         {
-    //             ArrayList<Cell> closedList = t.doThetaStar(grid);
-    //             DisplayGrid displayGrid = new DisplayGrid(grid, "2", closedList);
-    //             displayGrid.paintPath(closedList);
-    //         } else {
-    //             System.out.println("No Such Path");
-    //         }
-    //     // }
+            BFS bfs = new BFS();
+            ThetaStar t = new ThetaStar();
+            if(bfs.doBFS(grid))
+            {
+                ArrayList<Cell> closedList = t.doThetaStar(grid);
+                DisplayGrid displayGrid = new DisplayGrid(grid, "2", closedList);
+                displayGrid.paintPath(closedList);
+            } else {
+                System.out.println("No Such Path");
+            }
+        }
      }
 }
